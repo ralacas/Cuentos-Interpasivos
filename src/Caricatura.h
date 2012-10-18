2b3362e29f6cb5497ae5300dd068727687fc5867
ralacas <ra.acosta@gmail.com>
2012-10-18 17:45:54 -0500

    Este archivo no estaba dado de alta

--- src/Caricatura.h
+++ src/Caricatura.h
@@ -4,7 +4,17 @@
 //
 //  Created by Ra Acosta on 9/12/12.
 
+#include "ofMain.h"
+//#include "ofxOpenCv.h"
 
 class Caricatura{
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    //ofxCvColorImage colorImg;
     
 };
